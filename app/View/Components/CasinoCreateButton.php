e8a1aa96a78410f1fa5e10ccf8116205e53e61d6
tomislavp83 <prlictomislav83@gmail.com>
2021-03-07 16:33:39 +0100

    Created 'create new casino' button, home page update with new button

---
+++ app/View/Components/CasinoCreateButton.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class CasinoCreateButton extends Component
+{
+    /**
+     * Named route
+     *
+     * @var string
+     */
+    public $routeName;
+
+    /**
+     * Button text
+     *
+     * @var string
+     */
+    public $text;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($text, $routeName)
+    {
+       $this->text = $text;
+       $this->routeName = $routeName;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.casino-create-button');
+    }
+}
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,5 +2,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-casino-list :casinos="$casinos"/>
+    <div class="overflow-x-auto">
+        <div class="min-w-screen min-h-screen bg-gray-100 flex flex-col items-center justify-center bg-gray-100 font-sans overflow-hidden">
+            <x-casino-create-button :text="'Create casino'" :route-name="'casino.create'"/>
+
+            <x-casino-list :casinos="$casinos"/>
+
+            <x-casino-create-button :text="'Create casino'" :route-name="'casino.create'"/>
+        </div>
+    </div>
 @endsection
